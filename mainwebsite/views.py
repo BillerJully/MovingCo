2d80098e1376ee44d9095bbad6ffd51422ff72a7
zinnurov <zinnurov@yandex-team.ru>
2023-06-05 12:45:56 +0300

    added some html changes

--- mainwebsite/views.py
+++ mainwebsite/views.py
@@ -4,26 +4,27 @@ from django.http import HttpResponse, HttpResponseNotFound
 
 """redirect to home redirect('way', permanent = False)"""
 def mainpage(request):
-    return render(request, '')
+    return render(request, 'mainwebsite/mainpage.html')
 
 
 def makeoreder(request):
-    return render(request, '')
+    return render(request, 'mainwebsite/makeorder.html.html')
 
 
 def aboutus(request):
-    return render(request, '')
+    return render(request, 'mainwebsite/aboutus.html.html')
 
 
 def job(request):
-    return render(request, '')
-
-
-def pageNotFound(request, exception):
-    return render(request, '')
+    return render(request, 'mainwebsite/job.html')
 
 
 def clientfeedback(request):
-    return render(request, '')
+    return render(request, 'mainwebsite/clientsfeedback.html')
+
+
+def pageNotFound(request, exception):
+    return render(request, 'mainwebsite/pageNotFound.html')
+
 
 
